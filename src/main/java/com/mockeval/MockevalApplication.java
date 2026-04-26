59f1ba03bb19a1f0e8972c9b83dbfd5337e1bce2
pritish9156 <pritishpawar15@gmail.com>
2026-04-26 13:51:57 +0530

    Initial backend setup with project structure

---
+++ src/main/java/com/mockeval/MockevalApplication.java
@@ -0,0 +1,13 @@
+package com.mockeval;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MockevalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MockevalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mockeval/MockevalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mockeval;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MockevalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
